ccc35236cb8549bf212456b15eb2499ffedf5883
Estudiante <Estudiante@T203-20-2022>
2022-09-26 13:51:12 -0600

    Agregar archivos de proyecto.

---
+++ L5_RAJCL_1073522/Program.cs
@@ -0,0 +1,23 @@
+﻿int numeroEntero = 0;
+
+Console.WriteLine("EJERCICIO 1");
+
+Console.WriteLine("Numero Entero");
+
+
+numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+
+if (numeroEntero >= 1)
+{
+    Console.WriteLine("Numero positivo ");
+
+}
+else if (numeroEntero <= -1)
+{
+    Console.WriteLine("Numero Negativo");
+}
+else if (numeroEntero == 0)
+{
+    Console.WriteLine("Numero es cero");
+}
